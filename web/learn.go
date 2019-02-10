5c07d9aa0fddff762e79858029b3efb9a65675a2
Bay Dodd <baydodd@gmail.com>
2019-02-10 19:08:58 +0000

    Ensure support for only POST

--- web/learn.go
+++ web/learn.go
@@ -3,5 +3,9 @@ package web
 import "net/http"
 
 func LearnHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 	w.WriteHeader(http.StatusAccepted)
 }
--- web/learn_test.go
+++ web/learn_test.go
@@ -1,13 +1,14 @@
 package web_test
 
 import (
-	"github.com/burrbd/trigram/web"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 
 	"github.com/cheekybits/is"
+
+	"github.com/burrbd/trigram/web"
 )
 
 func TestLearnPostHandler(t *testing.T) {
@@ -16,14 +17,29 @@ func TestLearnPostHandler(t *testing.T) {
 	srv := httptest.NewServer(h)
 	defer srv.Close()
 	w := httptest.NewRecorder()
-	req:= httptest.NewRequest("POST", srv.URL,
+	req:= httptest.NewRequest(http.MethodPost, srv.URL,
 		strings.NewReader("To be or not to be, that is the question"))
 	req.Header.Set("Content-Type", "text/plain")
-
 
 	h.ServeHTTP(w, req)
 
 	n, _ := w.Result().Body.Read(make([]byte, 1))
 	is.Equal(0, n)
 	is.Equal(http.StatusAccepted, w.Result().StatusCode)
+}
+
+func TestLearnHandlerOnlyAcceptsPost(t *testing.T) {
+	is := is.New(t)
+	h := http.HandlerFunc(web.LearnHandler)
+	srv := httptest.NewServer(h)
+	defer srv.Close()
+	for _, method := range []string{"GET", "PUT", "PATCH", "DELETE", "HEAD", "CONNECT", "OPTIONS", "TRACE"} {
+		w := httptest.NewRecorder()
+		req:= httptest.NewRequest(method, srv.URL, nil)
+		req.Header.Set("Content-Type", "text/plain")
+
+		h.ServeHTTP(w, req)
+
+		is.Equal(http.StatusBadRequest, w.Result().StatusCode)
+	}
 }
