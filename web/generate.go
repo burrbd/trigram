4df0068066dcb8ab03efc7e96d8d1d34c2ee7447
Bay Dodd <baydodd@gmail.com>
2019-02-10 20:57:21 +0000

    Add generate handler

--- web/generate.go
+++ web/generate.go
@@ -1,1 +1,7 @@
 package web
+
+import "net/http"
+
+func GenerateHandler(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("To think it more than commonly anxious"))
+}
--- web/generate_test.go
+++ web/generate_test.go
@@ -1,1 +1,30 @@
-package web
+package web_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/cheekybits/is"
+
+	"github.com/burrbd/trigram/web"
+)
+
+func TestGenerateHandler(t *testing.T) {
+	is := is.New(t)
+
+	h := http.HandlerFunc(web.GenerateHandler)
+
+	srv := httptest.NewServer(h)
+	defer srv.Close()
+
+	req := httptest.NewRequest("GET", srv.URL, nil)
+	w := httptest.NewRecorder()
+
+	h.ServeHTTP(w, req)
+
+	expBody := "To think it more than commonly anxious"
+	actBody := w.Body.String()
+	is.Equal(expBody, actBody)
+	is.Equal(http.StatusOK, w.Result().StatusCode)
+}
